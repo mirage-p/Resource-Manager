950472c7e0851b4c76b53bcedb27947f0399aba3
Miraj Patel <mirajpatel@Mirajs-MacBook-Air.local>
2025-01-19 20:39:58 -0600

    setting up electron

---
+++ src/electron/main.js
@@ -0,0 +1,7 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+// This is too have the UI html run on electron
+app.on("ready", () => {
+  const mainWindow = new BrowserWindow({});
+  mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
+});
