201840f08f48e30cbd6ee15d19f27db1f25f8664
Miraj Patel <mirajpatel@Mirajs-MacBook-Air.local>
2025-01-19 22:16:06 -0600

    optimize npm scripts

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,8 +1,13 @@
 import { app, BrowserWindow } from "electron";
 import path from "path";
+import { isDev } from "./util.js";
 
 // This is too have the UI html run on electron
 app.on("ready", () => {
   const mainWindow = new BrowserWindow({});
-  mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
+  if (isDev()) {
+    mainWindow.loadURL("http://localhost:5123");
+  } else {
+    mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
+  }
 });
